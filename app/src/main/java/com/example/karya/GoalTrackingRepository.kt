13b11bd90da13d7a4a6b3d52728c10e2e0918357
nyanishinde <nyanishinde10@gmail.com>
2025-03-14 09:54:17 +0530

    Added DC,Dao,Repository,ViewModel for goal tracking table

--- app/src/main/java/com/example/karya/GoalTrackingRepository.kt
+++ app/src/main/java/com/example/karya/GoalTrackingRepository.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.LiveData
 class GoalTrackingRepository(private val trackingDoa: GoalTrackingDao) {
 
     suspend fun upsertProgress(progress: GoalTrackingDC){
-        trackingDoa.upsertProgress(tracking)
+        trackingDoa.upsertProgress(progress)
     }
     fun getTotalProgress(goalId: Int): LiveData<Int>{
         return trackingDoa.getTotalProgress(goalId)
