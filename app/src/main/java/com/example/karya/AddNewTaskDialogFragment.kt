dd585eb60f3a1881f02987cb50266facf214a18e
nyanishinde <nyanishinde10@gmail.com>
2025-03-18 08:30:53 +0530

    Added functionality of adding new image from bottom sheet

--- app/src/main/java/com/example/karya/AddNewTaskDialogFragment.kt
+++ app/src/main/java/com/example/karya/AddNewTaskDialogFragment.kt
@@ -16,7 +16,7 @@ class AddNewTaskDialogFragment: DialogFragment() {
     private lateinit var taskImage: ImageView
     private lateinit var imgAddBtn: ImageView
     private lateinit var taskName: EditText
-
+    private var selectedImageRes : Int= R.drawable.img_meditation
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.popup_add_new_task,container,false)
 
@@ -26,6 +26,15 @@ class AddNewTaskDialogFragment: DialogFragment() {
         imgAddBtn = view.findViewById(R.id.btnAddImage)
         taskName = view.findViewById(R.id.edtTaskName)
 
+        imgAddBtn.setOnClickListener {
+            val bottomSheet = ImagePickerBottomSheetDialog { selectedImage ->
+                selectedImageRes = selectedImage
+                taskImage.setImageResource(selectedImage)
+                taskImage.visibility = View.VISIBLE // Show selected image
+                imgAddBtn.visibility = View.GONE // Hide plus icon
+            }
+            bottomSheet.show(childFragmentManager, "ImagePickerBottomSheet")
+        }
 
         btnDone.setOnClickListener {
             dismiss()
